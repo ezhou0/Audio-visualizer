9e25e687bbb37b826be4f25b8fcfe4c830c01c8b
Edwin <edwinzhou99@gmail.com>
2021-07-22 13:46:43 -0700

    add waveform option and rendering capability

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ function visualize() {
         case 'classic':
             animatePlane();
             break;
+        case 'wave':
+            animateWave();
+            break;
     }
 }
 
@@ -113,22 +116,20 @@ function animatePlane(){
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
     let bufferLength = audioAnalyser.frequencyBinCount;
-    console.log(bufferLength);
-
     let dataArray = new Uint8Array(bufferLength);
 
     let WIDTH = canvas.width;
     let HEIGHT = canvas.height;
 
-    let bars = document.getElementById('barNum').value;
+    //let bars = document.getElementById('barNum').value;
 
     // let barWidth = document.getElementById('barWidth').value;
     let barWidth = (WIDTH / bufferLength) * 2.5;
     let barHeight;
     let x = 0;
 
-    function renderFrame() {
-        requestAnimationFrame(renderFrame);
+    function draw() {
+        requestAnimationFrame(draw);
 
         x = 0;
 
@@ -148,20 +149,61 @@ function animatePlane(){
         }
     }
 
-    renderFrame();
+    draw();
     
 }
 
-function drawPlane(x, y, freq, canvasContext, barHeight) {
-    //
-   
-    let barColor = document.getElementById('color').value;
+function animateWave(){
+    // //setup the canvas
+    let canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvasContext = canvas.getContext("2d");
 
-    // let barWidth = 2;
-    let barWidth = document.getElementById('barWidth').value;
+    // //canvas background
+    
 
-    canvasContext.fillStyle = barColor;
-    canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+    let bufferLength = audioAnalyser.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
+
+    let WIDTH = canvas.width;
+    let HEIGHT = canvas.height;
+
+    let bars = document.getElementById('barNum').value;
+    function draw(){
+        requestAnimationFrame(draw);
+        audioAnalyser.getByteTimeDomainData(dataArray);
+        let gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height); //x1, y1, x2, y2
+        gradient.addColorStop(0, "rgba(26, 13, 21, 1)")  //offset, color
+        gradient.addColorStop(1, "rgba(36, 45, 33, 1)")  //offset, color
+        canvasContext.fillStyle = gradient;
+        canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+        // canvasContext.lineWidth = 2;
+        canvasContext.lineWidth = document.getElementById('barWidth').value;
+        // canvasContext.strokeStyle = 'rgb(0, 0, 0)';
+        canvasContext.strokeStyle = document.getElementById('color').value;
+        canvasContext.beginPath();
+        //let sliceWidth = WIDTH * 1.0 / bufferLength;
+        let sliceWidth = WIDTH * 1.0 / bars;
+
+        let x = 0;
+        for (let i = 0; i < bufferLength; i++) {
+
+            let v = dataArray[i] / 128.0;
+            let y = v * HEIGHT / 2;
+
+            if (i === 0) {
+                canvasContext.moveTo(x, y);
+            } else {
+                canvasContext.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+        canvasContext.lineTo(canvas.width, canvas.height / 2);
+        canvasContext.stroke();
+    }
+    draw();
 }
 
 
