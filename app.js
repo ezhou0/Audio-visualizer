e8d13584f69894b9144ed1f9f8f57bdb14a2f776
Edwin <edwinzhou99@gmail.com>
2021-07-22 13:17:38 -0700

    fix classic style visualizer, now properly renders

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function visualize() {
     audioContext = new (window.AudioContext || window.webkitAudioContext);
 
     audioAnalyser = audioContext.createAnalyser();
-    // audio.src = "music/Adventures.mp3";
+   
     audio.src = document.getElementById('songChoice').value;
     let source = audioContext.createMediaElementSource(audio);
     source.connect(audioAnalyser);
@@ -22,11 +22,7 @@ function visualize() {
     document.getElementById('nav').style.display = 'flex';
     audio.play();
 
-    let canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvasContext = canvas.getContext("2d");
-    // animatePlane();
+   //select animation type
     let option = document.getElementById('visChoice').value;
     switch(option){
         case 'circle':
@@ -39,17 +35,11 @@ function visualize() {
 }
 
 function animateCircle() {
-    //setup the canvas
-    // let canvas = document.getElementById("canvas");
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-    // canvasContext = canvas.getContext("2d");
-
-    //center of the circle
-    let centerX = canvas.width / 2;
-    let centerY = canvas.height / 2;
-    let radius = 200;
    
+    let canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvasContext = canvas.getContext("2d");
 
     //background styling
     let gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height); //x1, y1, x2, y2
@@ -58,12 +48,17 @@ function animateCircle() {
     canvasContext.fillStyle = gradient;
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
+    //center of the circle
+    let centerX = canvas.width / 2;
+    let centerY = canvas.height / 2;
+    let radius = 200;
+   
     //Draw the circle
     canvasContext.beginPath();
     canvasContext.arc(centerX, centerY, radius, 0, 2 * Math.PI);
     canvasContext.stroke();
 
-    // const bars = 250;
+    // set number of bars to the slider value
     let bars = document.getElementById('barNum').value;
     const theta = 2 * Math.PI / bars;
     audioAnalyser.getByteFrequencyData(freqArray);
@@ -87,6 +82,7 @@ function animateCircle() {
 
 function drawBar(startX, startY, endX, endY, freq, canvasContext) {
     // barColor = "rgba(" + freq + ", " + freq + ", " + 205 + ")";
+
     let barColor = document.getElementById('color').value;
     
     // let barWidth = 2;
@@ -100,30 +96,60 @@ function drawBar(startX, startY, endX, endY, freq, canvasContext) {
     canvasContext.stroke();
 }
 
-function animatePlane(){
-    //setup the canvas
-    // let canvas = document.getElementById("canvas");
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-    // canvasContext = canvas.getContext("2d");
 
+function animatePlane(){
+    
+    //setup the canvas
+    let canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvasContext = canvas.getContext("2d");
+
+    //canvas background
     let gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height); //x1, y1, x2, y2
     gradient.addColorStop(0, "rgba(26, 13, 21, 1)")  //offset, color
     gradient.addColorStop(1, "rgba(36, 45, 33, 1)")  //offset, color
     canvasContext.fillStyle = gradient;
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+    let bufferLength = audioAnalyser.frequencyBinCount;
+    console.log(bufferLength);
+
+    let dataArray = new Uint8Array(bufferLength);
+
+    let WIDTH = canvas.width;
+    let HEIGHT = canvas.height;
+
     let bars = document.getElementById('barNum').value;
-    audioAnalyser.getByteFrequencyData(freqArray);
-    var bufferLength = audioAnalyser.frequencyBinCount;
-    
-    for (var i = 0; i < bars; i++) {
-        let barHeight = freqArray[i];
-        let x = 0;
-        let y = 0;
-        drawPlane(x, y, freqArray[i], canvasContext, barHeight);
-       x+=1;
+
+    // let barWidth = document.getElementById('barWidth').value;
+    let barWidth = (WIDTH / bufferLength) * 2.5;
+    let barHeight;
+    let x = 0;
+
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+
+        x = 0;
+
+        audioAnalyser.getByteFrequencyData(dataArray);
+
+        canvasContext.fillStyle = "#000";
+        canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
+
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+
+            // canvasContext.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+            canvasContext.fillStyle = document.getElementById('color').value;
+            canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+
+            x += barWidth + 1;
+        }
     }
-    requestAnimationFrame(animatePlane);
+
+    renderFrame();
+    
 }
 
 function drawPlane(x, y, freq, canvasContext, barHeight) {
